40bd5a49346173adda9b19f3ada8fb6b6c592498
IbrahemHurani <86603326+IbrahemHurani@users.noreply.github.com>
2021-11-18 19:48:17 +0200

    FileCall

--- Calls.py
+++ Calls.py
@@ -1,37 +1,18 @@
-import csv
+
 class Calls:
-     def __init__(self)->None:
-        self.AllTheCalls=[]
+    def __init__(self, data):
+        self.name = data[0]
+        self.time = float(data[1])
+        self.src = int(data[2])
+        self.dest = int(data[3])
+        self.state = int(data[4])
+        self.AllocateToChange = int(data[5])
+        self.finishedTime=-1
 
-     def ReadFrom_Csv(self,FileName)->None:
-        with open(FileName,"r+")as f:
-            My_dataReader=csv.reader(f)
-            for i in My_dataReader:
-                call_elevetor=Call(Time=float(i[1]),src=int(i[2]),dst=int(i[3]))
-                self.AllTheCalls.append(call_elevetor)
 
-     def Get_AllTheCalls(self):
-         return self.AllTheCalls
-
-class Call:
-    def __init__(self,Time:float=0,src: int=0,dst:int=0)->None:
-        self.Time=Time
-        self.src=src
-        self.dst=dst
-        self.elevetor="Elevetor Call"
-
-    def Get_TimeForElev(self)->float:
-        return self.Time
-
-    def Get_SrcForElev(self)->int:
-        return self.src
-
-    def Get_DstForElev(self)->int:
-        return self.dst
-
-    def __str__(self)->str:
-        return f"{self.elevetor},Time:{self.Time},src:{self.src},dst:{self.dst}"
-
-    def __repr__(self)->str:
-        return self.__str__()
+    def ChangeAloocate(self, elevator, building):
+        for elev in building.elevators:
+            if elevator.id == elev.id:
+                break
+        self.AllocateToChange = elevator.id
 

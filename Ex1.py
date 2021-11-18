7663d211c65dea297bf67d165d256b83e032ac05
IbrahemHurani <86603326+IbrahemHurani@users.noreply.github.com>
2021-11-19 00:22:23 +0200

    mainFile

--- Ex1.py
+++ Ex1.py
@@ -1,3 +1,65 @@
-from Bulding import Buliding
-from Calls import *
+import csv
+import subprocess
+import sys
+from Building import Building
+from Calls import Calls
+def get():
+    if len(sys.argv) == 4:
+        InFile = {
+            "building": sys.argv[1],
+            "calls": sys.argv[2],
+            "out": sys.argv[3]
+        }
+    else:
+        InFile = {
+            "building": "Ex1_input\\Ex1_Buildings\\B5.json",
+            "calls": "Ex1_input\\Ex1_Calls\\Calls_a.csv",
+            "out": "output.csv"
+        }
+    return InFile
 
+#this function for read from Csv
+def ReadCsvCalls(file_name):
+    calls = []
+    with open(file_name) as fp:
+        data = csv.reader(fp)
+        for i in data:
+            if (int(i[2]) < building.minFloor or int(i[2]) > building.maxFloor) and(int(i[3]) < building.minFloor or int(i[3]) > building.maxFloor):
+                raise Exception("Error Check The Floor")
+            calls.append(Calls(i))
+    return calls
+#this function to write new csv output
+def writeCsvCalls():
+    data = []
+    for i in calls:
+        data.append(i.__dict__.values())
+    with open(file["out"], 'w', newline="") as f:
+        csvwriter = csv.writer(f)
+        csvwriter.writerows(data)
+
+def Allocate():
+    elevators = building.elevators
+    Time = 0
+    for c in calls:
+        min = building.elevators[0]
+        for e in elevators:
+            e.removed()
+            if min.howMushTime(c) > e.howMushTime(c):
+                min = e
+        min.CallisHave.append(c)
+        c.Done = Time + min.howMushTime(c)
+        Time = c.time
+        c.ChangeAllocate(min, building)
+
+
+
+#tester for check
+def runTester(building, out):
+    subprocess.Popen(["powershell.exe", "java -jar Ex1_input\\Ex1_checker_V1.2_obf.jar 316203405,212187256 "+ building + "  "+ out + "  " + "Test.log"])
+if __name__ == "__main__":
+    file = get()
+    building = Building(file["building"])
+    calls = ReadCsvCalls(file["calls"])
+    Allocate()
+    writeCsvCalls()
+    runTester(file["building"],file["out"])

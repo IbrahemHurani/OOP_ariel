9eb9db9518fbe0db63fc3fd4c8486b293bfa7123
IbrahemHurani <86603326+IbrahemHurani@users.noreply.github.com>
2021-11-18 19:46:40 +0200

    fileBuilding

--- Bulding.py
+++ Bulding.py
@@ -1,34 +1,14 @@
 import json
-
-from Elevetor import Elevetor
+from Elevator import Elevator
 class Building:
-    def __init__(self,maxFloor:int=0,minFloor:int=0):
-        self.maxFloor=maxFloor
-        self.minFloor=minFloor
-        self.Elevetors=[]
+    def __init__(self,FileName):
+        with open(FileName, "r") as fp:
+            data = json.load(fp)
+            self.minFloor = int(data["_minFloor"])
+            self.maxFloor = int(data["_maxFloor"])
+            self.elevators = [Elevator(i) for i in data["_elevators"]]
 
-    def Read_json(self,FileName)->None:
-         try:
-            with open(FileName,"r+")as f:
-                data_buliding=json.load(f)
-                elevetor=[]
-                self.maxFloor=data_buliding["_maxFloor"]
-                self.minFloor=data_buliding["_minFloor"]
-                elev=data_buliding["_elevators"]
-                for i in elev:
-                    el=Elevetor(id=i["_id"],speed=i["_speed"],minFloor=i["_minFloor"],maxFloor=i["_maxFloor"],closeTime=i["_closeTime"],openTime=i["_openTime"],
-                                startTime=i["_startTime"],stopTime=i["_stopTime"])
-                    elevetor.append(el)
-                    self.Elevetors=elevetor
-         except IOError as e:
-             print(e)
 
-    def Get_ArrayElevetor(self):
-        return self.Elevetors
-    def Get_maxFloor(self)->int:
-        return self.maxFloor
-    def Get_minFloor(self)->int:
-        return self.minFloor
 
 
 
